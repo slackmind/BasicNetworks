56e850e4c9e6d5b52c7444104073641a14781dff
John <j44.young@connect.qut.edu.au>
2020-05-25 20:42:17 +1000

    starting client socket

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,18 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Client {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("localhost", 12345);
+
+        OutputStream outputStream = socket.getOutputStream();
+
+        outputStream.write(42);
+        
+        socket.close();
+
+    }
 }
